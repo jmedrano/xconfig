1e61ad2fce0ca4ecaef7d7409e6a7f0028dcb427
Jaime Medrano <jmedrano@tuenti.com>
2013-05-29 17:09:44 +0200

    XConfigUnixConnection implementation

--- src/libxconfig/xconfig_connection.cpp
+++ src/libxconfig/xconfig_connection.cpp
@@ -1,16 +1,44 @@
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/un.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #include <string>
 
 #include "xconfig_connection.h"
+#include "xconfig.h"
+
+using std::string;
 
 namespace xconfig {
 
-XConfigUnixConnection::XConfigUnixConnection(std::string path, std::string socket) : path(path), socket(socket), blob(0)
+const char* XConfigUnixConnection::WATCH_MSG = "watch";
+const char* XConfigUnixConnection::PUSH_MSG = "push";
+const char* XConfigUnixConnection::SEPARATOR = " ";
+const char* XConfigUnixConnection::TERMINATOR = "\r\n";
+const char* XConfigUnixConnection::DEFAULT_SOCKET = "/var/run/xconfig.socket";
+
+void MappedFile::reset(int fd) {
+	if (blob)
+		munmap(blob, size);
+	if (fd >= 0) {
+		struct stat st;
+		fstat(fd, &st);
+		size = st.st_size;
+		blob = mmap(0, size, PROT_READ, MAP_SHARED, fd, 0);
+	} else {
+		size = 0;
+		blob = 0;
+	}
+}
+
+XConfigUnixConnection::XConfigUnixConnection(std::string path, std::string socket) : path(path),
+		socket(socket.empty() ? DEFAULT_SOCKET: socket),
+		socket_fd(-1)
 {
 }
 
@@ -21,27 +49,86 @@ XConfigUnixConnection::~XConfigUnixConnection()
 
 bool XConfigUnixConnection::connect()
 {
-	// TODO
+	if (socket_fd < 0) {
+		// connect
+		struct sockaddr_un addr;
+		memset(&addr, 0, sizeof(addr));
+		addr.sun_family = AF_UNIX;
+		strncpy(addr.sun_path, socket.c_str(), sizeof(addr.sun_path)-1);
+
+		socket_fd = ::socket(AF_UNIX, SOCK_STREAM, 0);
+		if (socket_fd < 0)
+			throw XConfigNotConnected();
+		if (::connect(socket_fd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr)) < 0)
+			throw XConfigNotConnected();
+	}
+	if (!map) {
+		// send watch msg
+		string msg = string(WATCH_MSG) + SEPARATOR + path + TERMINATOR;
+		int written = ::write(socket_fd, msg.c_str(), msg.length());
+		if (written != static_cast<int>(msg.length()))
+			throw XConfigNotConnected();
+	}
+	if (socket_fd) {
+		// look for push msg
+		char data[1024], control[1024];
+		struct msghdr msg;
+		struct cmsghdr *cmsg;
+		struct iovec iov;
+		int tree_fd = -1;
+
+		// process all pending messages
+		for (;;) {
+			memset(&msg, 0, sizeof(msg));
+			iov.iov_base = data;
+			iov.iov_len = sizeof(data)-1;
+			msg.msg_iov = &iov;
+			msg.msg_iovlen = 1;
+			msg.msg_control = control;
+			msg.msg_controllen = sizeof(control);
+
+			// don't wait for push msg if a tree is already available
+			int msg_flags = map || tree_fd >= 0 ? MSG_CMSG_CLOEXEC | MSG_DONTWAIT : MSG_CMSG_CLOEXEC;
+			if (::recvmsg(socket_fd, &msg, msg_flags) < 0)
+				break;
+
+			data[iov.iov_len] = '\0';
+			// loop over all control msgs
+			cmsg = CMSG_FIRSTHDR(&msg);
+			while (cmsg) {
+				if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS) {
+					if (tree_fd >= 0) {
+						// more than one fd in msg. close every fd but the last one
+						::close(tree_fd);
+					}
+					tree_fd = *reinterpret_cast<int *>(CMSG_DATA(cmsg));
+				}
+				cmsg = CMSG_NXTHDR(&msg, cmsg);
+			}
+		}
+		if (tree_fd >= 0) {
+			// tree received. let's mmap it
+			map.reset(tree_fd);
+			::close(tree_fd);
+			return true;
+		}
+	}
 	return false;
 }
 
 void XConfigUnixConnection::close()
 {
-	if (fd > 0)
-		::close(fd);
-	if (blob)
-		munmap(blob, size);
-	blob = 0;
-	size = 0;
+	if (socket_fd >= 0)
+		::close(socket_fd);
+	map.reset();
 }
 
-const void* XConfigUnixConnection::get_blob()
+const void* XConfigUnixConnection::get_blob() const
 {
-	return blob;
+	return map.get_blob();
 }
 
-
-XConfigFileConnection::XConfigFileConnection(std::string path) : path(path), blob(0)
+XConfigFileConnection::XConfigFileConnection(std::string path) : path(path)
 {
 }
 
@@ -53,30 +140,24 @@ XConfigFileConnection::~XConfigFileConnection()
 bool XConfigFileConnection::connect()
 {
 	// don't reload
-	if (blob)
+	if (map)
 		return false;
 	int fd = open(path.c_str(), O_RDONLY);
 	if (fd < 0)
 		return false;
-	struct stat st;
-	fstat(fd, &st);
-	size = st.st_size;
-	blob = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+	map.reset(fd);
 	::close(fd);
 	return true;
 }
 
 void XConfigFileConnection::close()
 {
-	if (blob)
-		munmap(blob, size);
-	blob = 0;
-	size = 0;
+	map.reset();
 }
 
-const void* XConfigFileConnection::get_blob()
+const void* XConfigFileConnection::get_blob() const
 {
-	return blob;
+	return map.get_blob();
 }
 
 } // namespace xconfig
--- src/libxconfig/xconfig_connection.h
+++ src/libxconfig/xconfig_connection.h
@@ -2,43 +2,69 @@
 #define LIBXCONFIG_XCONFIG_CONNECTION_H_
 
 #include <string>
+#include <boost/noncopyable.hpp>
 
 namespace xconfig {
 
-class XConfigConnection {
+class MappedFile : private boost::noncopyable {
+public:
+	MappedFile() : size(0), blob(0) {}
+	explicit MappedFile(int fd) : size(0), blob(0) {
+		reset(fd);
+	}
+	~MappedFile() {
+		reset();
+	}
+	void reset(int fd = -1);
+	const void* get_blob() const {
+		return blob;
+	}
+	operator bool() const {
+		return blob;
+	}
+private:
+	size_t size;
+	void* blob;
+};
+
+class XConfigConnection : private boost::noncopyable {
 public:
 	virtual ~XConfigConnection() {}
 	virtual bool connect() = 0;
 	virtual void close() = 0;
-	virtual const void* get_blob() = 0;
+	virtual const void* get_blob() const = 0;
 };
 
 class XConfigUnixConnection : public XConfigConnection {
 public:
+	static const char* WATCH_MSG;
+	static const char* PUSH_MSG;
+	static const char* SEPARATOR;
+	static const char* TERMINATOR;
+	static const char* DEFAULT_SOCKET;
+
 	XConfigUnixConnection(std::string path, std::string socket = "");
 	virtual ~XConfigUnixConnection();
 	virtual bool connect();
 	virtual void close();
-	virtual const void* get_blob();
+	virtual const void* get_blob() const;
 private:
 	std::string path;
 	std::string socket;
-	int fd;
-	size_t size;
-	void* blob;
+	int socket_fd;
+	MappedFile map;
 };
 
 class XConfigFileConnection : public XConfigConnection {
 public:
-	XConfigFileConnection(std::string path);
+	explicit XConfigFileConnection(std::string path);
 	virtual ~XConfigFileConnection();
 	virtual bool connect();
 	virtual void close();
-	virtual const void* get_blob();
+	virtual const void* get_blob() const;
 private:
 	std::string path;
-	size_t size;
-	void* blob;
+	MappedFile map;
 };
 
 } // namespace xconfig
