feae4ff86cad892972535b57f7f8b6e55c898f26
Daniel Pañeda <dpaneda@tuenti.com>
2020-05-20 18:14:41 +0200

    Add steps to create whl for python 3.8

--- bindings/python/build.sh
+++ bindings/python/build.sh
@@ -6,3 +6,11 @@ apt update
 apt-get -y install python3-pip
 pip3 install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
+
+# Instructions to get a wheel for python 3.8
+#apt install wget https://people.debian.org/~paravoid/python-all/unofficial-python-all.asc
+#wget https://people.debian.org/~paravoid/python-all/unofficial-python-all.asc
+#echo "deb http://people.debian.org/~paravoid/python-all $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/python-all.list
+#apt update
+#apt install python3.8
+#python3.8 setup.py sdist bdist_wheel
